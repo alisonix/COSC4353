411b545d4fa910a826a52fc42e1a6b6a77857400
RicardoSGzz <99140779+Jrsanch6@users.noreply.github.com>
2022-09-18 04:13:07 +0000

    Grid with cells

--- main.py
+++ main.py
@@ -9,54 +9,44 @@ pygame.init()
 WIDTH = 400
 HEIGHT = 300
 ROWS = 20
-PIXEL_WIDTH = (WIDTH / ROWS)
-PIXEL_HEIGHT = (HEIGHT / ROWS)
 DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
 
-#Create array
-a = np.random.rand(ROWS, ROWS)
+#Create random array
+randomArray = np.random.rand(ROWS, ROWS)
 
-
+#Function to draw on the screen
 def draw(width, cell_height, cell_width, surface, rows):
-    x = 0
-    y = 0
-    j = 0
-    k = 0
+  xCoordinate = 0
+  yCoordinate = 0
 
-    for i in range(rows):
-        x += cell_width
-        y += cell_height
+  #for loop to draw the grid
+  for i in range(rows):
+    xCoordinate += cell_width
+    yCoordinate += cell_height
 
-        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, width))
+    pygame.draw.line(surface, (255, 255, 255), (xCoordinate, 0), (xCoordinate, width))
 
-        pygame.draw.line(surface, (255, 255, 255), (0, y), (width, y))
-        #print(a[j][k])
+    pygame.draw.line(surface, (255, 255, 255), (0, yCoordinate), (width, yCoordinate))
 
-
-#        if (a[j, k] > 0.5):
-#            surf = pygame.Surface((WIDTH / ROWS, HEIGHT / ROWS))
-#            surf.fill((255, 255, 255))
-#            DISPLAYSURF.blit(surf, (x, y))
-
-    for j in range(ROWS):
+  #for loop to iterate through the nd array
+  for j in range(ROWS):
         for k in range(ROWS):
-            if (a[j, k] > 0.8):
+          #If the value of the random array is more than 0.8, draw a cell in those coordinates
+            if (randomArray[j, k] > 0.8):
                 surf = pygame.Surface((cell_width, cell_height))
                 surf.fill((255, 255, 255))
                 DISPLAYSURF.blit(surf, ((j * cell_width), (k * cell_height)))
 
-
+#main function
 def main():
+  while True:
+    draw(WIDTH, HEIGHT / ROWS, WIDTH / ROWS, DISPLAYSURF, ROWS)
 
-    while True:
+    for event in pygame.event.get():
+      if event.type == QUIT:
+        pygame.quit()
+        sys.exit()
+    pygame.display.update()
 
-        draw(WIDTH, HEIGHT / ROWS, WIDTH / ROWS, DISPLAYSURF, ROWS)
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
-
-
-main()
+if __name__ == "__main__":
+  main()
