b45da6edcffc658833ba5be254a427169c404d91
Laney Tran <98193301+alisonix@users.noreply.github.com>
2022-09-22 00:47:16 +0000

    updating to current version 9.21

--- Assignment1/src/gameoflife.py
+++ Assignment1/src/gameoflife.py
@@ -1,8 +1,11 @@
-class GameOfLife:
-  True
+import enum
 
-class Cell:
-  status = True
-  
-  def isAlive(status):
-    return Cell.status
+class CellState(enum.Enum):
+    ALIVE = True
+    DEAD = False
+
+def next_state(cell_status, number_of_live_neighbors):
+  return number_of_live_neighbors == 3 or cell_status == CellState.ALIVE.value and number_of_live_neighbors == 2
+
+def grid_status():
+  return False
--- Assignment1/test/test_game_of_life.py
+++ Assignment1/test/test_game_of_life.py
@@ -1,22 +1,72 @@
 import unittest
 
-#from src.gameoflife import GameOfLife
-from src.gameoflife import Cell
+from src.gameoflife import *
+
+DEAD = CellState.DEAD.value
+ALIVE = CellState.ALIVE.value
 
 class GameOfLifeTests(unittest.TestCase):
-  def test_Canary(self):
-    self.assertTrue(True)
 
-  def test_Cell_exists(self):
-    self.assertTrue(True)
+    def test_canary(self):
+        self.assertTrue(True)
 
-  def test_Cell_is_Alive(self):
-    Cell.status = True
-    self.assertEqual(True, Cell().isAlive())
+    def test_dead_cell_dead_no_neighbors(self):
+        cell_status = DEAD
+        number_of_live_neighbors = 0
+        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
 
-  def test_Cell_is_Dead(self):
-    Cell.status = False
-    self.assertEqual(False, Cell().isAlive())
+    def test_dead_cell_dead_one_neighbor(self):
+        cell_status = DEAD
+        number_of_live_neighbors = 1
+        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
 
-if __name__ == '__main__': 
-  unittest.main()
+    def test_dead_cell_dead_two_neighbors(self):
+        cell_status = DEAD
+        number_of_live_neighbors = 2
+        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
+
+    def test_dead_cell_dead_five_neighbors(self):
+        cell_status = DEAD
+        number_of_live_neighbors = 5
+        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
+
+    def test_dead_cell_dead_eight_neighbors(self):
+        cell_status = DEAD
+        number_of_live_neighbors = 8
+        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
+
+    def test_dead_cell_alive_three_neighbors(self):
+        cell_status = DEAD
+        number_of_live_neighbors = 3
+        self.assertEqual(ALIVE, next_state(cell_status, number_of_live_neighbors))
+
+    def test_live_cell_dead_one_neighbor(self):
+        cell_status = ALIVE
+        number_of_live_neighbors = 1
+        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
+
+    def test_live_cell_dead_four_neighbors(self):
+        cell_status = ALIVE
+        number_of_live_neighbors = 4
+        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
+
+    def test_live_cell_dead_eight_neighbors(self):
+        cell_status = ALIVE
+        number_of_live_neighbors = 8
+        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
+
+    def test_live_cell_live_three_neighbors(self):
+        cell_status = ALIVE
+        number_of_live_neighbors = 3
+        self.assertEqual(ALIVE, next_state(cell_status, number_of_live_neighbors))
+
+    def test_live_cell_live_two_neighbors(self):
+      cell_status = ALIVE
+      number_of_live_neighbors = 2
+      self.assertEqual(ALIVE, next_state(cell_status, number_of_live_neighbors))
+
+    def test_grid_does_not_exist(self):
+      self.assertEqual(False, grid_status())
+
+if __name__ == '__main__':
+    unittest.main()
