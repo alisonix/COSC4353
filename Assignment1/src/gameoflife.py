426c84de98ac4b33f970cb131f0d998c55e9ac7a
RicardoSGzz <99140779+Jrsanch6@users.noreply.github.com>
2022-09-27 21:12:18 +0000

    Modified code and tests.txt

--- Assignment1/src/gameoflife.py
+++ Assignment1/src/gameoflife.py
@@ -1,5 +1,4 @@
 import enum
-from enum import Enum, auto
 
 class CellState(enum.Enum):
     ALIVE = True
@@ -8,7 +7,30 @@ class CellState(enum.Enum):
 def next_state(cell_status, number_of_live_neighbors):
   return number_of_live_neighbors == 3 or cell_status == CellState.ALIVE.value and number_of_live_neighbors == 2
 
-def generate_signals_for_cell(cell): #2, 3
-  # ((1, 2), (1, 3), (1, 4), (2, 2), (2, 4), (3, 2), (3, 3), (3, 4))
-  neighbor_signals = ((cell[0]-1, cell[1]-1), (cell[0]-1, cell[1]), (cell[0]-1, cell[1] + 1), (cell[0], cell[1]-1), (cell[0], cell[1]+1), (cell[0] + 1, cell[1] - 1), (cell[0]+1, cell[1]), (cell[0]+1, cell[1]+1))
-  return neighbor_signals
+def generate_signals_for_a_cell(cell):
+  x = cell[0] 
+  y = cell[1]
+  return tuple((x + j, y + i)
+    for j in range(-1, 2)
+    for i in range (-1, 2)
+    if not (x + j == x and y + i == y))
+
+def generate_signals_for_multiple_cells(cells):
+  return [item for i in [generate_signals_for_a_cell(x) for x in cells] for item in i]
+
+def count_live_neighbors(cells):
+  return {i: cells.count(i) for i in cells}
+
+def compute_next_generation(cells):
+  nextGen = [] 
+  for j, k in count_live_neighbors(generate_signals_for_multiple_cells(cells)).items():
+    Status = False
+    if any(l == j for l in list(cells)): Status = True
+    if (next_state(Status, k) == True): nextGen.insert(0, j)
+  return tuple(nextGen)
+
+  
+  #return tuple([nextGen 
+   #         for j, k in count_live_neighbors(generate_signals_for_multiple_cells(cells)).items()
+    #          if any(l == j for l in list(cells)): Status = True
+     #         if (next_state(Status, k) == True): nextGen.insert(0, j)])
--- Assignment1/test/test_game_of_life.py
+++ Assignment1/test/test_game_of_life.py
@@ -5,86 +5,103 @@ from src.gameoflife import *
 DEAD = CellState.DEAD.value
 ALIVE = CellState.ALIVE.value
 
-
 class GameOfLifeTests(unittest.TestCase):
 
     def test_canary(self):
         self.assertTrue(True)
 
-    def test_dead_cell_dead_no_neighbors(self):
-        cell_status = DEAD
-        number_of_live_neighbors = 0
-        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
+    def test_dead_cell_behavior(self):
+      number_of_live_neighbors_and_next_state = [(0, DEAD), (1, DEAD), (2, DEAD), (5, DEAD), (8, DEAD), (3, ALIVE)]
+      for number_of_live_neighbors, next_cell_state in number_of_live_neighbors_and_next_state:
+        with self.subTest(msg = "next_cell_state for number_of_live_neighbors", number_of_live_neighbors=number_of_live_neighbors):          
+          self.assertEqual(next_cell_state, next_state(DEAD, number_of_live_neighbors))
 
-    def test_dead_cell_dead_one_neighbor(self):
-        cell_status = DEAD
-        number_of_live_neighbors = 1
-        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
-
-    def test_dead_cell_dead_two_neighbors(self):
-        cell_status = DEAD
-        number_of_live_neighbors = 2
-        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
-
-    def test_dead_cell_dead_five_neighbors(self):
-        cell_status = DEAD
-        number_of_live_neighbors = 5
-        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
-
-    def test_dead_cell_dead_eight_neighbors(self):
-        cell_status = DEAD
-        number_of_live_neighbors = 8
-        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
-
-    def test_dead_cell_alive_three_neighbors(self):
-        cell_status = DEAD
-        number_of_live_neighbors = 3
-        self.assertEqual(ALIVE, next_state(cell_status, number_of_live_neighbors))
-
-    def test_live_cell_dead_one_neighbor(self):
-        cell_status = ALIVE
-        number_of_live_neighbors = 1
-        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
-
-    def test_live_cell_dead_four_neighbors(self):
-        cell_status = ALIVE
-        number_of_live_neighbors = 4
-        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
-
-    def test_live_cell_dead_eight_neighbors(self):
-        cell_status = ALIVE
-        number_of_live_neighbors = 8
-        self.assertEqual(DEAD, next_state(cell_status, number_of_live_neighbors))
-
-    def test_live_cell_live_three_neighbors(self):
-        cell_status = ALIVE
-        number_of_live_neighbors = 3
-        self.assertEqual(ALIVE, next_state(cell_status, number_of_live_neighbors))
-
-    def test_live_cell_live_two_neighbors(self):
-        cell_status = ALIVE
-        number_of_live_neighbors = 2
-        self.assertEqual(ALIVE, next_state(cell_status, number_of_live_neighbors))
-
+    def test_live_cell_behavior(self):
+      number_of_live_neighbors_and_next_state = [(1, DEAD), (4, DEAD), (8, DEAD), (3, ALIVE), (2, ALIVE)]
+      for number_of_live_neighbors, next_cell_state in number_of_live_neighbors_and_next_state:
+        with self.subTest(msg = "next_cell_state for number_of_live_neighbors", number_of_live_neighbors=number_of_live_neighbors):          
+          self.assertEqual(next_cell_state, next_state(ALIVE, number_of_live_neighbors))
+          
     def test_live_cell_at_2_3(self):
         cell = (2, 3)
         self.assertEqual(
-            ((1, 2), (1, 3), (1, 4), (2, 2), (2, 4), (3, 2), (3, 3), (3, 4)), generate_signals_for_cell(cell))
+            ((1, 2), (1, 3), (1, 4), (2, 2), (2, 4), (3, 2), (3, 3), (3, 4)),
+            generate_signals_for_a_cell(cell))
 
     def test_live_cell_at_3_3(self):
         cell = (3, 3)
         self.assertEqual(
-            ((2, 2), (2, 3), (2, 4), (3, 2), (3, 4), (4, 2), (4, 3), (4, 4)), generate_signals_for_cell(cell))
+            ((2, 2), (2, 3), (2, 4), (3, 2), (3, 4), (4, 2), (4, 3), (4, 4)),
+            generate_signals_for_a_cell(cell))
 
     def test_live_cell_at_2_4(self):
         cell = (2, 4)
         self.assertEqual(
-            ((1, 3), (1, 4), (1, 5), (2, 3), (2, 5), (3, 3), (3, 4), (3, 5)), generate_signals_for_cell(cell))
+            ((1, 3), (1, 4), (1, 5), (2, 3), (2, 5), (3, 3), (3, 4), (3, 5)),
+            generate_signals_for_a_cell(cell))
 
     def test_live_cell_at_0_0(self):
         cell = (0, 0)
-        self.assertEqual(((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)), generate_signals_for_cell(cell))
+        self.assertEqual(((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1),
+                          (1, 0), (1, 1)), generate_signals_for_a_cell(cell))
 
+    def test_given_no_positions(self):
+      cells = ()
+      self.assertEqual([], generate_signals_for_multiple_cells(cells))
+
+    def test_given_one_positions(self):
+      cells = ((0, 0), )
+      self.assertEqual([(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)],
+                        generate_signals_for_multiple_cells(cells))
+      
+    def test_given_two_positions(self):
+      cells = ((0, 0), (2, 3))
+      self.assertEqual([(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1), 
+                        (1, 2), (1, 3), (1, 4), (2, 2), (2, 4), (3, 2), (3, 3), (3, 4)], 
+                        generate_signals_for_multiple_cells(cells))
+      
+    def test_given_three_positions(self):
+      cells = ((0, 0), (2, 3), (3, 3))
+      self.assertEqual([(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1), 
+                        (1, 2), (1, 3), (1, 4), (2, 2), (2, 4), (3, 2), (3, 3), (3, 4), 
+                        (2, 2), (2, 3), (2, 4), (3, 2), (3, 4), (4, 2), (4, 3), (4, 4)],     
+                        generate_signals_for_multiple_cells(cells))
+
+    def test_count_live_neighbors_no_positions(self):
+      cells = ()
+      self.assertEqual({}, count_live_neighbors(cells))
+
+    def test_count_live_neighbors_one_position(self):
+      cells = ((0, 0), )
+      self.assertEqual({(0, 0): 1}, count_live_neighbors(cells))
+
+    def test_count_live_neighbors_two_position(self):
+      cells = ((0, 0), (0, 0))
+      self.assertEqual({(0, 0): 2}, count_live_neighbors(cells))
+      
+    def test_count_live_neighbors_three_position(self):
+      cells = ((0, 0), (0, 1), (0, 0))
+      self.assertEqual({(0, 0): 2, (0, 1): 1}, count_live_neighbors(cells))
+
+    def test_block_compute_next_generation(self):
+      cells = ((0, 0), (0, 1), (1, 0), (1, 1))
+      self.assertEqual(((0, 0), (1, 1), (1, 0), (0, 1)), compute_next_generation(cells))
+      
+    def test_hive_compute_next_generation(self):
+      cells = ((0,1), (1,0), (2, 0), (3, 1), (2, 2), (1, 2))
+      self.assertEqual(((2, 2), (3, 1), (2, 0), (0, 1), (1, 2), (1, 0)), compute_next_generation(cells))
+
+    def test_horizontal_blinker_compute_next_generation(self):
+      cells = ((0, 0), (1, 0), (2, 0))
+      self.assertEqual(((1, 1), (1, 0), (1, -1)), compute_next_generation(cells))
+
+    def test_vertical_blinker_compute_next_generation(self):
+      cells = ((1, 1), (1, 0), (1, -1))
+      self.assertEqual(((2, 0), (1, 0), (0, 0)), compute_next_generation(cells))
+
+    def test_glider_compute_next_generation(self):
+      cells = ((1, 0), (2, 1), (2, 2), (1, 2), (0, 2))
+      self.assertEqual(((1, 3), (2, 2), (1, 2), (2, 1), (0, 1)), compute_next_generation(cells))
 
 if __name__ == '__main__':
     unittest.main()
